d37c12e1a214e1e839da82493184ae452c199ef9
Fred Marques <fredyn.curso@gmail.com>
2025-05-23 17:09:59 -0300

    commit

---
+++ API/Controllers/System/SystemController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.System;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SystemController : ControllerBase
+{
+    private readonly ISystemService _systemService;
+
+    public SystemController(ISystemService systemService)
+    {
+        _systemService = systemService;
+    }
+}
